59d1e8925c0c91f34480004a3a2b4140d71dd044
Boyan-Apostolov <boian4934@gmail.com>
2021-07-09 14:08:26 +0300

    Create header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import * as material from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './components/shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+
+import { AppRoutingModule } from '../../app-routing.module';
+
+@NgModule({
+  declarations: [FooterComponent, HeaderComponent],
+  imports: [CommonModule, AppRoutingModule],
+  exports: [HeaderComponent, FooterComponent],
+})
+export class SharedModule {}
