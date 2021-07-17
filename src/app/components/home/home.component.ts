2fd43c7186da30984213d1cbe9b7e2fb06652566
Boyan-Apostolov <boian4934@gmail.com>
2021-07-17 17:24:02 +0300

    Add animation to home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,62 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('headingState', [
+      state(
+        'moveLeft',
+        style({ transform: 'translate3d(-20px, 0, 0)', offset: 1 })
+      ),
+      state(
+        'moveRight',
+        style({ transform: 'translate3d(10px, 0, 0)', offset: 1 })
+      ),
+      transition('* => *', animate('1000ms ease')),
+    ]),
+  ],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  position: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    interval(1000)
+      .pipe()
+      .subscribe(() => {
+        this.moveLeft();
+      });
+    interval(2000)
+      .pipe()
+      .subscribe(() => {
+        this.moveRight();
+      });
   }
 
+  resetPosition() {
+    this.changePosition('null');
+  }
+
+  moveLeft() {
+    this.changePosition('moveLeft');
+  }
+
+  moveRight() {
+    this.changePosition('moveRight');
+  }
+
+  changePosition(newPosition: string) {
+    this.position = newPosition;
+  }
 }
