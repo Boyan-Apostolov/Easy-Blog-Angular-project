c3dac97591fb688d434ac6b5bfe5106df05c36c8
Boyan-Apostolov <boian4934@gmail.com>
2021-07-25 18:32:54 +0300

    Add animations to blog hovering

--- src/app/components/blogs/blog-item/blog-item.component.ts
+++ src/app/components/blogs/blog-item/blog-item.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit, Input } from '@angular/core';
 import { Blog } from 'src/app/core/models/blog/blog';
 import { BlogService } from 'src/app/core/services/blog/blog.service';
@@ -6,16 +13,32 @@ import { UserService } from 'src/app/core/services/user/user-service.service';
   selector: 'app-blog-item',
   templateUrl: './blog-item.component.html',
   styleUrls: ['./blog-item.component.css'],
+  animations: [
+    trigger('imageState', [
+      state('zoomIn', style({ transform: 'scale(1.12)', offset: 1 })),
+      transition('* => *', animate('1000ms ease')),
+    ]),
+  ],
 })
 export class BlogItemComponent implements OnInit {
   @Input() blog!: Blog;
-
+  position: string = '';
   constructor(
     private blogService: BlogService,
     private userService: UserService
   ) {}
 
   ngOnInit(): void {}
+
+  zoomIn() {
+    this.changePosition('zoomIn');
+  }
+  zoomOut() {
+    this.changePosition('null');
+  }
+  changePosition(newPosition: string) {
+    this.position = newPosition;
+  }
 
   incrementBlogViews(blog: Blog) {
     let userId = this.userService.currentUser.id;
