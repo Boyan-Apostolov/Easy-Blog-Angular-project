8aece4ca345b6ba53d3877ea2528941d580ff510
Boyan-Apostolov <boian4934@gmail.com>
2021-07-25 19:04:00 +0300

    Add blogs module and clean up app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,80 +3,62 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
-import { MatChipsModule } from '@angular/material/chips';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SharedModule } from './components/shared/shared.module';
-import { HomeComponent } from './components/home/home.component';
-
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 
-import { CKEditorModule } from 'ng2-ckeditor';
-
 import { ShareModule } from 'ngx-sharebuttons';
-import { AdminModule } from './components/admin/admin.module';
-
 import { environment } from '../environments/environment';
 import { AuthGuard } from './core/guards/auth.guard';
 
+import { AuthModule } from './components/auth/auth.module';
+import { AdminModule } from './components/admin/admin.module';
+import { BlogsModule } from './components/blogs/blogs.module';
+import { SharedModule } from './components/shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AuthRoutingModule } from './components/auth/auth-routing.module';
 import { AdminRoutingModule } from './components/admin/admin-routing.module';
 import { BlogsRoutingModule } from './components/blogs/blogs-routing.module';
 
 import { BlogService } from './core/services/blog/blog.service';
+import { ChatService } from './core/services/chat/chat.service';
 import { UserService } from './core/services/user/user-service.service';
 import { ImageUploadService } from './core/services/image-upload/image-upload.service';
 
-import { BlogsComponent } from './components/blogs/blogs/blogs.component';
-import { BlogItemComponent } from './components/blogs/blog-item/blog-item.component';
-import { BlogArticleComponent } from './components/blogs/blog-article/blog-article.component';
-import { BlogNewComponent } from './components/blogs/blog-new/blog-new.component';
+import { AppComponent } from './app.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { HomeComponent } from './components/home/home.component';
 import { PrivacyComponent } from './components/privacy/privacy.component';
-import { BlogsByTagComponent } from './components/blogs/blogs-by-tag/blogs-by-tag.component';
 import { UserProfileComponent } from './components/user/user-profile/user-profile.component';
 import { LeaderboardComponent } from './components/user/leaderboard/leaderboard.component';
-import { ChatComponent } from './components/chat/chat.component';
-import { ChatService } from './core/services/chat/chat.service';
-import { AuthModule } from './components/auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    BlogsComponent,
-    BlogItemComponent,
-    BlogArticleComponent,
-    BlogNewComponent,
     PrivacyComponent,
-    BlogsByTagComponent,
     UserProfileComponent,
     LeaderboardComponent,
     ChatComponent,
   ],
   imports: [
     BrowserModule,
-    AuthModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ShareModule,
     AppRoutingModule,
     AuthRoutingModule,
     AdminRoutingModule,
     BlogsRoutingModule,
+    AuthModule,
     SharedModule,
     AdminModule,
-    BrowserAnimationsModule,
-    CKEditorModule,
-    FormsModule,
+    BlogsModule,
     AngularFireModule.initializeApp(environment.firebase, 'easy-blog'),
     AngularFirestoreModule,
     AngularFireAuthModule,
     AngularFireStorageModule,
-    MatChipsModule,
-    MatProgressSpinnerModule,
-    ShareModule,
   ],
   providers: [
     BlogService,
---
+++ src/app/components/blogs/blogs.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BlogsRoutingModule } from './blogs-routing.module';
+import { BlogArticleComponent } from './blog-article/blog-article.component';
+import { BlogItemComponent } from './blog-item/blog-item.component';
+import { BlogNewComponent } from './blog-new/blog-new.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { BlogsByTagComponent } from './blogs-by-tag/blogs-by-tag.component';
+import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatChipsModule } from '@angular/material/chips';
+import { CKEditorModule } from 'ng2-ckeditor';
+
+@NgModule({
+  declarations: [
+    BlogArticleComponent,
+    BlogItemComponent,
+    BlogNewComponent,
+    BlogsComponent,
+    BlogsByTagComponent,
+  ],
+  imports: [
+    CommonModule,
+    BlogsRoutingModule,
+    SharedModule,
+    FormsModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
+    CKEditorModule,
+    BlogsRoutingModule,
+  ],
+  exports: [BlogItemComponent],
+})
+export class BlogsModule {}
