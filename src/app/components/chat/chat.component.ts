636a80ed9504158990f400dafdffd7f44371fbcc
Boyan-Apostolov <boian4934@gmail.com>
2021-07-29 19:01:23 +0300

    Design changes

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { interval } from 'rxjs';
 import { ChatMessage } from 'src/app/core/models/chat/chat';
 import { ChatService } from 'src/app/core/services/chat/chat.service';
@@ -40,13 +41,15 @@ export class ChatComponent {
     return e.setDate(e.getDate() - 1);
   }
 
-  postHandler() {
-    if (this.messageContent != '') {
+  postHandler(formData: NgForm) {
+    this.messageContent = formData.controls.message.value;
+    if (this.messageContent && this.messageContent != '') {
       this.chatService.postMessage(this.messageContent);
     } else {
       this.addAlert('Error!', 'Message content cannot be empty!', 'danger');
     }
     this.messageContent = '';
+    formData.controls.message.reset();
   }
 
   addAlert(heading: string, message: string, alertClass: string) {
